128d495b44b1cdadde8c23c6c60bb7a2c728a3cf
Erik Gabriel <126505086+erikgabriel07@users.noreply.github.com>
2024-03-17 11:34:43 -0300

    Add files via upload

--- source/main.py
+++ source/main.py
@@ -1,6 +1,12 @@
 from src.config import app
 from src.routes import api_router
+
+from fastapi import Request, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from uvicorn import run
+
 
 app.include_router(api_router)
 
@@ -8,6 +14,15 @@ app.include_router(api_router)
 @app.get("/")
 def home():
     return {"message": "Welcome to my application!"}
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_error_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder(
+            {"detail": exc.errors(), "message": "Invalid name field!"})
+    )
 
 
 if __name__ == "__main__":
